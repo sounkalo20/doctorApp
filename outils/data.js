4c7cd451a9275138f654ea377ffecf20283cf76f
sounkalo20 <sidibesounk2003@gmail.com>
2023-11-14 12:12:07 +0000

    ok

---
+++ outils/data.js
@@ -0,0 +1,25 @@
+export const FakeActivity = [
+    {
+        id: 1,
+        mainText: 'Docteur a Domicile',
+        subText: 'Votre docteur a Domicile'
+    },
+    {
+        id: 2,
+        mainText: 'Clinique a votre disposition',
+        subText: 'Une Clinique pour vous soigner a tout moment'
+    },
+    {
+        id: 3,
+        mainText: 'Traitement de Qualiter',
+        subText: 'Vous aurez avec nous les meilleurs soins'
+    }, {
+        id: 4,
+        mainText: 'Traitement de Qualiter',
+        subText: 'Vous aurez avec nous les meilleurs soins'
+    }, {
+        id: 5,
+        mainText: 'Traitement de Qualiter',
+        subText: 'Vous aurez avec nous les meilleurs soins'
+    }
+]
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,14 +1,91 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  FlatList,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import { PADDING } from "../outils/constantes";
+import { FakeActivity } from "../outils/data";
+import DoctorSvg from './../assets/doctor1.svg'
+import { Svg, SvgUri } from "react-native-svg";
 
 const HomeScreen = () => {
   return (
-    <View style={{flex: 1}}>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    <ScrollView>
+      {/* Debut du header */}
+      <View style={styles.header}>
+        <Text style={styles.userName}>Jhon Doe</Text>
+        <Image
+          source={require("./../assets/profil.jpg")}
+          style={styles.userImage}
+        />
+      </View>
+      {/* fin du header */}
 
-export default HomeScreen
+      {/* liste des activites */}
+      <FlatList
+        horizontal={true}
+        data={FakeActivity}
+        keyExtractor={(item) => item.id}
+        showsHorizontalScrollIndicator={false}
+        style={styles.flatListeContainer}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity style={styles.flatListeItem}>
+              <SvgUri width="200" height="200" uri={DoctorSvg}/>
+              <Text style={styles.mainText}>{item.mainText}</Text>
+              <Text style={styles.subText}>{item.subText}</Text>
+            </TouchableOpacity>
+          );
+        }}
+      />
+    </ScrollView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: PADDING.horizontal,
+    paddingVertical: PADDING.vertical,
+    backgroundColor: "white",
+    paddingTop: 50,
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50 / 2,
+  },
+  userName: {
+    fontSize: 17,
+    fontWeight: "bold",
+  },
+  flatListeContainer : {
+    paddingHorizontal : PADDING.horizontal,
+    paddingVertical: PADDING.vertical
+  },
+  flatListeItem : {
+    paddingHorizontal: 15,
+    backgroundColor : 'white',
+    paddingVertical : 15,
+    marginRight: 15,
+    elevation: 5,
+    borderRadius: 10
+  },
+  mainText : {
+    fontWeight : 'bold',
+    fontSize: 15
+  },
+  subText : {
+    marginTop : 10,
+    fontSize : 12
+  }
+});
